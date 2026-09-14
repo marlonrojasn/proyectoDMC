fc52d66a28ec0081dd8b04d72004c75ecdfe0ccc
marlonrojasn <marlonrojas2403@gmail.com>
2026-09-14 03:46:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import streamlit as st
 st.title ("TRABAJO PRÁCTICO MÓDULO I")
-st.sidebar.selectbox ("Desplegar",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
+Módulos = st.sidebar.selectbox ("Desplegar",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
+if Módulos = "Home"
+  st.write ("Trabajo Módulo I")
+  st.write ("Marlon Rojas Novoa")
 st.write("Elaborado por: Marlon Rojas")
