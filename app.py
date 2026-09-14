410b7d65b244775ce2303c758cdd84f43b5c4a0e
marlonrojasn <marlonrojas2403@gmail.com>
2026-09-14 03:42:38 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title ("TRABAJO PRÁCTICO MÓDULO I")
-st.sidebar.selectbox ("Home",["Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
+st.sidebar.selectbox ("Desplegar",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
 st.write("Elaborado por: Marlon Rojas")
