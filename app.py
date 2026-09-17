409ec168eb9c3bcc6ed6bb59a80aae684f9641a0
marlonrojasn <marlonrojas2403@gmail.com>
2026-09-17 18:42:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import numpy as np
 st.title ("TRABAJO PRÁCTICO MÓDULO I")
-st.sidebar.title("Módulos")
-st.sidebar.image("Python.png", width=150)
+st.sidebar.markdown(
+    "<h2 style='text-align: center;'>Módulos</h2>",
+    unsafe_allow_html=True)
+st.sidebar.image("prog.png", width=150)
 Módulos = st.sidebar.selectbox ("Desplegar",["Home","Movimientos Financieros","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
 if Módulos == "Home":
   st.write ("Trabajo Módulo I") 
