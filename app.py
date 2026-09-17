c2678a98357f9301847e469373f195301cd70533
marlonrojasn <marlonrojas2403@gmail.com>
2026-09-17 11:58:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,9 @@ if Módulos == "Home":
 elif Módulos =="Movimientos Financieros":
   st.write ("Resolución Ejercicio1")
   st.markdown("Vamos Resolver El Ejercicio1")
+  st.text_input("Ingrese Movimiento")
+  Tipo = select.box("Tipo de Movimiento",[Ingreso,Gasto])
+  valor = st.number_input( "Ingrese el valor", min_value=0.0, value=0.0)
   
 
 elif Módulos =="Ejercicio 2":
