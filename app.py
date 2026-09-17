d34e30d50611c04bcbfbfc32e822352e4b654788
marlonrojasn <marlonrojas2403@gmail.com>
2026-09-16 19:24:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,15 @@ if Módulos == "Home":
   st.write ("Trabajo Módulo I") 
   st.write ("Marlon Rojas Novoa")
   st.image("Python.png")
+elif Módulos =="Ejercicio 1
+  st.write ("Resolución Ejercicio1")
+
+  cantidad =st.slider("Selecciones un valor del rango", min_value = 1, maxvalue = 100, value=20)
+  arreglo =np.arrange(cantidad)
+
+  st.write(arreglo)
+
+elif Módulos =="Ejercicio 2"
+elif Módulos =="Ejercicio 3"
+ 
 st.write("Desarrollo web - Python")
