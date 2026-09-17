e49ee831c228aa794268a6dcba133f562e84cdbf
marlonrojasn <marlonrojas2403@gmail.com>
2026-09-16 19:34:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ st.title ("TRABAJO PRÁCTICO MÓDULO I")
 Módulos = st.sidebar.selectbox ("Desplegar",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
 if Módulos == "Home":
   st.write ("Trabajo Módulo I") 
-  st.write ("Marlon Rojas Novoa")
-  st.image("Python.png")
+   st.image("Python.png")
+  st.write ("Marlon Jerson Rojas Novoa")
+  st.write ("Módulo I")
+  st.write("Ingeniero con interés en tecnología, análisis y gestión de datos, orientado al aprendizaje continuo y la innovación")
+  st.write ("Año: 2026")
+  st.write ("Descripción del proyecto")
+  st.write ("Tecnologías usadas")
+
 elif Módulos =="Ejercicio 1"
   st.write ("Resolución Ejercicio1")
-
   cantidad =st.slider("Selecciones un valor del rango", min_value = 1, maxvalue = 100, value=20)
   arreglo =np.arrange(cantidad)
 
