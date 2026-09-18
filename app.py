8f1be8cf176619490e6f1bdec29bfa3f4ad5026e
marlonrojasn <marlonrojas2403@gmail.com>
2026-09-18 11:46:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -71,4 +71,5 @@ elif Módulos == "Movimientos Financieros":
     else:
         st.info("No tiene saldo a favor ni en contra")
   
-
+elif Módulos = "Ejercicio 2"
+    st.write ="Andrea a como el kg"
